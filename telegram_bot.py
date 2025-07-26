b4bbc4ceb94b22cc28c8c99f3eb85b3903e369fb
Illia061 <147086513+Illia061@users.noreply.github.com>
2025-07-26 18:26:07 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,8 +2,8 @@
 from telegram import Bot
 import os
 
-bot = Bot(token=os.getenv('8454330473:AAFh0heBBs1NhfKlVUQH7IUbcUdIBpOcf9o'))
+bot = Bot(token=os.getenv("TELEGRAM_BOT_TOKEN"))
 
 def post_to_telegram(text):
-    chat_id = os.getenv('@fasterfootballnews')
+    chat_id = os.getenv("TELEGRAM_CHANNEL_ID")
     bot.send_message(chat_id=chat_id, text=text, parse_mode="HTML")
